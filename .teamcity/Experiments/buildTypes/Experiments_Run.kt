5db93985aa4cb2580d8be53f95c7936f56ae822b
Aleksandr Oleynikov <aoleynikov@avito.ru>
2018-02-13 23:07:32 +0300

    add vcs in Run build

--- .teamcity/Experiments/buildTypes/Experiments_Run.kt
+++ .teamcity/Experiments/buildTypes/Experiments_Run.kt
@@ -1,6 +1,7 @@
 package Experiments.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v10.*
+import jetbrains.buildServer.configs.kotlin.v10.triggers.vcs
 
 object Experiments_Run : BuildType({
     uuid = "08806c58-4c9e-47a3-8079-c524ae236b66"
@@ -11,6 +12,20 @@ object Experiments_Run : BuildType({
             snapshot {
                 reuseBuilds = ReuseBuilds.NO
             }
+        }
+    }
+
+    vcs {
+        root(Experiments.vcsRoots.Experiments_HttpsGithubComBigaltumTeamcityDebugGit)
+
+        checkoutMode = CheckoutMode.MANUAL
+    }
+
+    triggers {
+        vcs {
+            id = "vcsTrigger"
+            triggerRules = ""
+            branchFilter = ""
         }
     }
 })
